09719dcc23e282d5a5620d37a91660bb527015f4
shervintafreshi <shervintafreshipour@gmail.com>
2021-10-26 23:47:27 -0400

    Fixed syntactic issues with server code

--- managers/db_manager.py
+++ managers/db_manager.py
@@ -4,7 +4,7 @@ import datetime
 from peewee import *
 
 # Initialize the database connection
-db = SqliteDatabase('btn_database.db')
+db = SqliteDatabase('./database/btn_database.db')
 
 # Define Meta Table 
 class BaseModel(Model):
@@ -20,13 +20,13 @@ Primary Table Definitions:
 
 class User(BaseModel):
     user_id = AutoField(primary_key=True)
-    username = charField(unique=True)
-    email = charField(unique=True)
-    password = charField()
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField()
 
 class Story(BaseModel):
     story_id = AutoField(primary_key=True)
-    title = charField(unique=True)
+    title = CharField(unique=True)
     content =  TextField()
 
 class Comment(BaseModel):
@@ -44,28 +44,28 @@ db.create_tables([User, Story, Comment])
 
 
 # Story Table Transactions
-def add_story(title: str, content: str):
+def add_story(title: str, content: str) -> Story:
     return Story.create(title=title, content=content)
 
-def get_story_by_id(story_id: int):
+def get_story_by_id(story_id: int) -> Story:
     return Story.get(Story.story_id == story_id)
 
-def get_all_stories():
+def get_all_stories() -> list[Story]:
     return Story.select()
 
 # User Table Transactions
-def get_user_by_username(username: str):
+def get_user_by_username(username: str) -> User:
     return User.get(User.username == username)
 
-def get_user_by_id(user_id: int):
+def get_user_by_id(user_id: int) -> User:
     return User.get(User.user_id == user_id)
 
-def add_user(username: str, email: str, password: str):
+def add_user(username: str, email: str, password: str) -> User:
     return User.create(username=username, email=email, password=hashlib.sha512(password).hexdigest())
 
 # Comment Table Transactions
-def add_comment(content: str, user_id: int, story_id: int):
+def add_comment(content: str, user_id: int, story_id: int) -> Comment:
     return Comment.create(content=content, user_id=user_id, story_id=story_id)
 
-def get_comment_by(comment_id: str):
+def get_comment_by(comment_id: str) -> Comment:
     return Comment.create(Comment.comment_id == comment_id)
--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 import hashlib, uuid
 import json, jwt
+from managers.db_manager import *
 from pydantic import BaseModel
 from typing import Optional
 from fastapi import FastAPI, Response, Cookie
-from managers.db_manager import *
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
 # Generate fast API object
 app = FastAPI()
@@ -14,14 +16,14 @@ class Comment(BaseModel):
     user_id: int
     story_id: int
 
-class Credientials(BaseModel):
+class Credentials(BaseModel):
     username: str
     password: str
 
 # Primary API Routes
 @app.get("/")
 async def read_root():
-    return {"Name": "BTN Term Project API"
+    return {"Name": "BTN Term Project API",
             "Version": "1.0", 
             "Author": "Shervin Tafreshipour",
             "Description": """backend API constructed to handle the various requests
@@ -29,23 +31,30 @@ async def read_root():
                            """
             }
 
+# Request a single story object
 @app.get("/stories/{item_id}")
 async def read_item(item_id: int, q: Optional[str] = None):
-    return get_story_by_id(item_id) 
+    story = get_story_by_id(item_id)
+    return JSONResponse(content=jsonable_encoder(story))
 
+# Request all user stories
 @app.get("/stories/all")
 async def get_stories():
-    return get_all_stories()
+    stories = get_all_stories()
+    return JSONResponse(content=jsonable_encoder(stories))
 
+# Request to add comment to story
 @app.post("/stories/add_comment")
 async def add_story_comment(comment: Comment):
-    return add_story_comment(comment['content'], comment['user_id'], comment['story_id'])
-    
+    comment = add_story_comment(comment.content, comment.user_id, comment.story_id)
+    return JSONResponse(content=jsonable_encoder(comment))
+
+# Request to add user login   
 @app.post("/user/login")
 async def user_login(response: Response, credentials: Credentials):
     response_content = None
     user = get_user_by_username(credentials.username)
-    if user and user.password == hashlib.sha512(credentials['password']).hexdigest():
+    if user and user.password == hashlib.sha512(credentials.password).hexdigest():
         response_content = {"authenticated": True}
         jwt_token = jwt.encode({"exp": datetime.now(tz=timezone.utc)} + datetime.timedelta(minutes=10), "SECRET_KEY", algorithm='HS256')
         response.set_cookie(key='token', value=jwt_token)
@@ -53,6 +62,7 @@ async def user_login(response: Response, credentials: Credentials):
         response_content = {"authenticated": False}
     return response_content
 
+# Request to authenticate JWT token
 @app.post("/account/authenticate")
 async def user_authentication(token: Optional[str] = Cookie(None)):
     response_content = None
@@ -65,6 +75,7 @@ async def user_authentication(token: Optional[str] = Cookie(None)):
         response_content = {"authenticated": False}
     return response_content
 
+# Request to logout user
 @app.get("/account/logout")
 async def user_logout(token: Optional[str] = Cookie(None)):
     jwt_token = jwt.encode({"exp": datetime.now(tz=timezone.utc)}, "SECRET_KEY", algorithm='HS256')
