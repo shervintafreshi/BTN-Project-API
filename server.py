6093604d167f87863876c2220f6f6526460b1158
unknown <shervintafreshipour@gmail.com>
2021-12-02 21:07:25 -0500

    Added CRSF Protection to app

--- server.py
+++ server.py
@@ -58,10 +58,20 @@ class Story(BaseModel):
     content: str
     comment: Comment
 
+# Test Route
+@app.get("/csrftoken/")
+async def get_csrf_token(csrf_protect: CsrfProtect = Depends()):
+	response = JSONResponse(status_code=200, content={'csrf_token':'cookie'})
+	csrf_protect.set_csrf_cookie(response)
+	return response
 
 # Primary API Routes
 @app.get("/")
-async def read_root():
+async def read_root(request: Request, csrf_protect: CsrfProtect = Depends()):
+    
+    # test Statement
+    csrf_protect.validate_csrf_in_cookies(request)
+
     return JSONResponse({"Name": "BTN Term Project API",
                          "Version": "1.5",
                          "Author": "Shervin Tafreshipour",
